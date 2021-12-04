3350b16365787e3dcb0267bc02c376c524b5935a
stockpenguin <60495695+stockpenguin@users.noreply.github.com>
2021-12-03 22:08:45 -0600

    Create Background.java

---
+++ Background.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+
+public class Background {
+    private int width, height;
+    private Color color;
+
+    public Background(int width, int height, Color color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
